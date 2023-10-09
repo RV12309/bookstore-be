4e45ffa1dbac1844562f0e13dab63f59bed1c9a8
Nguyen Hoang Yen <yen.nh184023@sis.hust.edu.vn>
2023-10-09 17:14:08 +0700

    init project

---
+++ bookstore/src/test/java/com/hust/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hust.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
